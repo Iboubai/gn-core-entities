342e4be2cd930e7a151fcfef3310bcb4e189c194
idoumbouya <doumbouyaibrahima@gmail.com>
2025-09-21 17:41:14 +0000

    Initiation des entités de location (region, prefecture et SousPrefecture)

---
+++ gn-core-entities/Location/Prefecture.cs
@@ -0,0 +1,32 @@
+﻿namespace gn_core_entities.Location
+{
+    public class Prefecture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public long Population { get; private set; }
+        public int AreaKm2 { get; private set; }
+        public string Longitude { get; private set; }
+        public string Latitude { get; private set; }
+        public int RegionId { get; set; }
+
+        public Prefecture()
+        {
+            
+        }
+
+        public Prefecture(int id, string name, string code, long population, int areaKm2, string longitude, string latitude, int regionId)
+        {
+
+            Id = id;
+            Name = name;
+            Code = code;
+            Population = population;
+            AreaKm2 = areaKm2;
+            Longitude = longitude;
+            Latitude = latitude;
+            RegionId = regionId;
+        }
+    }
+}
---
+++ gn-core-entities/Location/Region.cs
@@ -0,0 +1,29 @@
+﻿namespace gn_core_entities.Location
+{
+    public class Region
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public long Population { get; private set; }
+        public int AreaKm2 { get; private set; }
+        public string Longitude { get; private set; }
+        public string Latitude { get; private set; }
+
+        public Region()
+        {
+        }
+
+        public Region(int id, string name, string code, long population, int areaKm2, string longitude, string latitude)
+        {
+            Id = id;
+            Name = name;
+            Code = code;
+            Population = population;
+            AreaKm2 = areaKm2;
+            Longitude = longitude;
+            Latitude = latitude;
+        }
+    }
+}
